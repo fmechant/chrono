22f6abefa91a97546e8db8f1b3c7649ad32c3d73
fmechant <fmechant@bitimprove.eu>
2019-03-10 21:11:46 +0100

    Initial version of DateAndTime

--- src/Chrono/DateAndTime.elm
+++ src/Chrono/DateAndTime.elm
@@ -1,20 +1,38 @@
-module Chrono.DateAndTime exposing (DateAndTime)
+module Chrono.DateAndTime exposing
+    ( DateAndTime
+    , fromMoment
+    , now
+    )
 
 import Chrono.Date as Date exposing (Date)
 import Chrono.Moment as Moment exposing (Moment)
 import Chrono.Time as Time exposing (Time)
 import Chrono.TimeZone as TimeZone exposing (TimeZone)
+import Task exposing (Task)
+import Time as CoreTime
 
 
+{-| A date and time.
+-}
 type alias DateAndTime =
     { date : Date
     , time : Time
     }
 
 
-{-| -}
-fromMoment : TimeZone -> Time -> Moment -> DateAndTime
-fromMoment zone time moment =
+{-| Get the DateAndTime that represents the moment in this time zone.
+-}
+fromMoment : TimeZone -> Moment -> DateAndTime
+fromMoment zone moment =
     { date = Date.fromMoment zone moment
     , time = Time.fromMoment zone moment
     }
+
+
+{-| Get the date and time at the moment when this task is run and in the time zone
+where this task is run.
+-}
+now : Task x DateAndTime
+now =
+    Task.map2 fromMoment (Task.map TimeZone.withSameOffset CoreTime.here) <|
+        Task.map (Moment.fromMsSinceEpoch << CoreTime.posixToMillis) CoreTime.now
--- tests/Chrono/DateAndTimeTests.elm
+++ tests/Chrono/DateAndTimeTests.elm
@@ -1,16 +1,29 @@
 module Chrono.DateAndTimeTests exposing (all)
 
+import Chrono.Date as Date
 import Chrono.DateAndTime exposing (..)
 import Chrono.Moment as Moment
-import Chrono.Time
+import Chrono.Time as Time
+import Chrono.TimeZone as TimeZone
 import Expect
 import Test exposing (..)
-import Time
 
 
 all : Test
 all =
-    describe "Date tests"
-        [ describe "fromMoment should use correct conversion to JDN."
-            []
+    describe "Date and Time tests"
+        [ describe "fromMoment"
+            [ test "should give the same date and time as the specific fromMoments" <|
+                \() ->
+                    let
+                        moment =
+                            Moment.fromMsSinceEpoch 120000
+
+                        timeZone =
+                            TimeZone.utc
+                    in
+                    moment
+                        |> fromMoment timeZone
+                        |> Expect.equal { date = Date.fromMoment timeZone moment, time = Time.fromMoment timeZone moment }
+            ]
         ]
