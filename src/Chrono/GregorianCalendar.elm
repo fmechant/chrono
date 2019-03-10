48fbb6339839fcb3207d63ac1de55cea89a19eac
fmechant <fmechant@bitimprove.eu>
2019-03-10 18:50:48 +0100

    initial GregorianCalendar with simple moves

--- src/Chrono/GregorianCalendar.elm
+++ src/Chrono/GregorianCalendar.elm
@@ -1,58 +1,302 @@
-module Chrono.GregorianCalendar exposing (toDate, toMonthNumber)
+module Chrono.GregorianCalendar exposing
+    ( Duration
+    , Month(..)
+    , andThen
+    , days
+    , fromDayMonthYear
+    , fromMonthNumber
+    , intoFuture
+    , intoPast
+    , months
+    , toDayMonthYear
+    , toMonthNumber
+    , years
+    )
 
-import Chrono.Date as Date
-import Time
+import Chrono.Date as Date exposing (Date)
+
+
+
+---- Conversion
 
 
 {-| Convert a year, month and day on the gregorian calendar to a date.
+
+It uses the algoritm described in <https://en.wikipedia.org/wiki/Julian_day>
+
 -}
-toDate : Int -> Time.Month -> Int -> Date.Date
-toDate y month d =
+fromDayMonthYear : { day : Int, month : Month, year : Int } -> Date.Date
+fromDayMonthYear { day, month, year } =
     let
         m =
             toMonthNumber month
     in
-    Date.fromJDN <| (1461 * (y + 4800 + (m - 14) // 12)) // 4 + (367 * (m - 2 - 12 * ((m - 14) // 12))) // 12 - (3 * ((y + 4900 + (m - 14) // 12) // 100)) // 4 + d - 32075
+    Date.fromJDN <| (1461 * (year + 4800 + (m - 14) // 12)) // 4 + (367 * (m - 2 - 12 * ((m - 14) // 12))) // 12 - (3 * ((year + 4900 + (m - 14) // 12) // 100)) // 4 + day - 32075
+
+
+{-| Convert a date to the year, month and day on the gregorian calendar.
+-}
+toDayMonthYear : Date -> { day : Int, month : Month, year : Int }
+toDayMonthYear date =
+    let
+        j =
+            Date.toJDN date
+
+        f =
+            j + 1401 + (((4 * j + 274277) // 146097) * 3) // 4 - 38
+
+        e =
+            4 * f + 3
+
+        g =
+            modBy 1461 e // 4
+
+        h =
+            5 * g + 2
+
+        d =
+            modBy 153 h // 5 + 1
+
+        m =
+            modBy 12 (h // 153 + 2) + 1
+
+        y =
+            (e // 1461) - 4716 + (12 + 2 - m) // 12
+    in
+    { day = d, month = fromMonthNumber m, year = y }
+
+
+
+---- Month
+
+
+{-| The month in the gregorian calendar.
+-}
+type Month
+    = January
+    | February
+    | March
+    | April
+    | May
+    | June
+    | July
+    | August
+    | September
+    | October
+    | November
+    | December
 
 
 {-| Convert the month to a number representing the month.
 1 is January, 12 is December.
 -}
-toMonthNumber : Time.Month -> Int
+toMonthNumber : Month -> Int
 toMonthNumber month =
     case month of
-        Time.Jan ->
+        January ->
             1
 
-        Time.Feb ->
+        February ->
             2
 
-        Time.Mar ->
+        March ->
             3
 
-        Time.Apr ->
+        April ->
             4
 
-        Time.May ->
+        May ->
             5
 
-        Time.Jun ->
+        June ->
             6
 
-        Time.Jul ->
+        July ->
             7
 
-        Time.Aug ->
+        August ->
             8
 
-        Time.Sep ->
+        September ->
             9
 
-        Time.Oct ->
+        October ->
             10
 
-        Time.Nov ->
+        November ->
             11
 
-        Time.Dec ->
+        December ->
             12
+
+
+{-| Convert the month number to a month.
+1 is January, 12 is December.
+-}
+fromMonthNumber : Int -> Month
+fromMonthNumber number =
+    case modBy 12 number of
+        1 ->
+            January
+
+        2 ->
+            February
+
+        3 ->
+            March
+
+        4 ->
+            April
+
+        5 ->
+            May
+
+        6 ->
+            June
+
+        7 ->
+            July
+
+        8 ->
+            August
+
+        9 ->
+            September
+
+        10 ->
+            October
+
+        11 ->
+            November
+
+        _ ->
+            December
+
+
+
+---- Move ----
+
+
+{-| Move the date a duration into the future.
+-}
+intoFuture : Duration -> Date -> Date
+intoFuture (Duration durations) date =
+    durations
+        |> List.foldl move date
+
+
+intoPast : Duration -> Date -> Date
+intoPast (Duration durations) date =
+    durations
+        |> List.map negate
+        |> List.foldl move date
+
+
+move : DurationItem -> Date -> Date
+move item date =
+    case item of
+        NumberOfDays noDays ->
+            date
+                |> Date.intoFuture (Date.days noDays)
+
+        NumberOfMonths noMonths ->
+            let
+                dmy =
+                    toDayMonthYear date
+
+                monthsWithoutYears =
+                    remainderBy 12 noMonths
+
+                aPrioriYears =
+                    noMonths // 12
+
+                newMonthMinusOne =
+                    toMonthNumber dmy.month + monthsWithoutYears - 1
+
+                yearsDiffWhenPositieve =
+                    newMonthMinusOne // 12
+
+                yearDiff =
+                    if newMonthMinusOne < 0 then
+                        yearsDiffWhenPositieve - 1
+
+                    else
+                        yearsDiffWhenPositieve
+            in
+            fromDayMonthYear
+                { day = dmy.day
+                , month = fromMonthNumber <| 1 + modBy 12 newMonthMinusOne
+                , year = dmy.year + yearDiff + aPrioriYears
+                }
+
+        NumberOfYears noYears ->
+            let
+                dmy =
+                    toDayMonthYear date
+            in
+            fromDayMonthYear { day = dmy.day, month = dmy.month, year = dmy.year + noYears }
+
+
+negate : DurationItem -> DurationItem
+negate item =
+    case item of
+        NumberOfDays value ->
+            NumberOfDays -value
+
+        NumberOfMonths value ->
+            NumberOfMonths -value
+
+        NumberOfYears value ->
+            NumberOfYears -value
+
+
+
+---- DURATION ----
+
+
+{-| Duration represents a laps of time. It is represented in the date and time model,
+because we are thinking about actual elaps of specific days, months and years.
+-}
+type Duration
+    = Duration (List DurationItem)
+
+
+type DurationItem
+    = NumberOfDays Int
+    | NumberOfMonths Int
+    | NumberOfYears Int
+
+
+days : Int -> Duration
+days value =
+    Duration [ NumberOfDays value ]
+
+
+months : Int -> Duration
+months value =
+    Duration [ NumberOfMonths value ]
+
+
+years : Int -> Duration
+years value =
+    Duration [ NumberOfYears value ]
+
+
+{-| Combine two durations.
+
+It has an odd signiture to be able to efficiently use it using the pipe (|>) operator.
+Example:
+
+    years 2
+        |> andThen days 15
+        |> viewDuration
+    --> { days: 15, months: 0, years: 2}
+
+-}
+andThen : (Int -> Duration) -> Int -> Duration -> Duration
+andThen fct value (Duration durations) =
+    let
+        (Duration toAdd) =
+            fct value
+    in
+    Duration (durations ++ toAdd)
--- tests/Chrono/GregorianCalendarTests.elm
+++ tests/Chrono/GregorianCalendarTests.elm
@@ -1,19 +1,74 @@
 module Chrono.GregorianCalendarTests exposing (all)
 
-import Chrono.Date as Date
-import Chrono.GregorianCalendar as Cal
+import Chrono.Date as Date exposing (Date)
+import Chrono.GregorianCalendar exposing (..)
 import Chrono.Moment as Moment
 import Expect
+import Fuzz
+import Random
 import Test exposing (..)
 import Time
 
 
 all : Test
 all =
-    describe "toDate returns correct date."
-        [ test "toDate of January 1, 2000 in utc is JDN 2,451,545. " <|
-            \() ->
-                Cal.toDate 2000 Time.Jan 1
-                    |> Date.toJDN
-                    |> Expect.equal 2451545
+    describe "Gregorian Calendar Tests"
+        [ describe "conversions"
+            [ test "fromDayMonthYear of January 1, 2000 in utc is the correct date. " <|
+                \() ->
+                    fromDayMonthYear { year = 2000, month = January, day = 1 }
+                        |> Expect.equal firstJanuary2000
+            , test "toDayMonthYear returns the correct day, month and year. " <|
+                \() ->
+                    twentyEightFebruary2000
+                        |> toDayMonthYear
+                        |> Expect.equal { year = 2000, month = February, day = 28 }
+            ]
+        , describe "moving dates"
+            [ test "into the future returns the correct date" <|
+                \() ->
+                    firstJanuary2000
+                        |> intoFuture (days 58)
+                        |> Expect.equal twentyEightFebruary2000
+            , fuzz fuzzDate "0 months in the future is the same date" <|
+                \aDate ->
+                    aDate
+                        |> intoFuture (months 0)
+                        |> Expect.equal aDate
+            , test "takes the order of the duration into account" <|
+                \() ->
+                    twentyEightFebruary2000
+                        |> intoFuture (days 3 |> andThen months 3)
+                        |> Expect.equal (fromDayMonthYear { day = 2, month = June, year = 2000 })
+            , fuzz2 fuzzDate fuzzDuration "moving into future and then the same into past is staying here." <|
+                \date duration ->
+                    date
+                        |> intoFuture duration
+                        |> intoPast duration
+                        |> Expect.equal date
+            ]
+        ]
+
+
+firstJanuary2000 : Date
+firstJanuary2000 =
+    Date.fromJDN 2451545
+
+
+twentyEightFebruary2000 : Date
+twentyEightFebruary2000 =
+    Date.fromJDN (2451545 + 58)
+
+
+fuzzDate : Fuzz.Fuzzer Date
+fuzzDate =
+    Fuzz.map Date.fromJDN <| Fuzz.intRange 2415021 3415021
+
+
+fuzzDuration : Fuzz.Fuzzer Duration
+fuzzDuration =
+    Fuzz.oneOf
+        [ Fuzz.map days <| Fuzz.intRange 0 Random.maxInt
+        , Fuzz.map months <| Fuzz.intRange 0 1000
+        , Fuzz.map years <| Fuzz.intRange 0 40
         ]
