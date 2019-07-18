c9c21a908b0cf531e6123a568fa216ad74d69b4f
fmechant <fmechant@bitimprove.eu>
2019-07-18 08:44:34 +0200

    add the Hour type and enable to construct time using am, pm, h24 and m
    The API enables us to do: am 7 |> m 15 -- 07:15:00
    We opted not to use string parsing because we want to prevent unnecessary Maybe's or Result's.

--- src/Chrono/Time.elm
+++ src/Chrono/Time.elm
@@ -1,6 +1,14 @@
 module Chrono.Time exposing
-    ( Time
+    ( Hour
+    , Time
+    , am
     , fromMoment
+    , h24
+    , m
+    , midnight
+    , ms
+    , noon
+    , pm
     , toMsSinceNoon
     )
 
@@ -22,6 +30,13 @@ type Time
     = Time Int
 
 
+{-| An incomplete time type that only contains the hour of the time.
+Use m to complete it to a Time type.
+-}
+type Hour
+    = Hour Int
+
+
 fromMoment : TimeZone -> Moment -> Time
 fromMoment zone moment =
     Time <|
@@ -30,5 +45,110 @@ fromMoment zone moment =
 
 
 toMsSinceNoon : Time -> Int
-toMsSinceNoon (Time ms) =
-    ms
+toMsSinceNoon (Time milliseconds) =
+    milliseconds
+
+
+{-| Construct the time using an hour AM.
+It needs m to make it a complete Time.
+
+Values below 1 are considered as 1, values above 12 are considered as 12.
+
+12:00 AM is midnight.
+
+Example:
+
+    am 7 |> m 15 -- 7:15:00,000
+
+-}
+am : Int -> Hour
+am =
+    clamp 1 12 >> midnightNoonCorrection >> h24
+
+
+{-| Construct the time using an hour PM.
+It needs m to make it a complete Time.
+
+Values below 1 are considered as 1, values above 12 are considered as 12.
+
+12:00 PM is noon.
+
+Example:
+
+    pm 7 |> m 15 -- 19:15:00,000
+
+-}
+pm : Int -> Hour
+pm =
+    clamp 1 12 >> midnightNoonCorrection >> (+) 12 >> h24
+
+
+{-| The correction to make sure 12:00 AM is midnight and 12:00 PM is noon.
+-}
+midnightNoonCorrection : Int -> Int
+midnightNoonCorrection int =
+    case int of
+        12 ->
+            0
+
+        _ ->
+            int
+
+
+{-| Construct the time using an hour on the 24-hours clock.
+It needs m to make it a complete Time.
+
+We avoid using 24 because of the confusion involved.
+So, midnight is 0:00:00,000 and not 24:00:00,000.
+
+Values below 0 are considered as 0, values above 23 are considered as 23.
+
+Example:
+
+    h24 19 |> m 15 -- 19:15:00,000
+
+-}
+h24 : Int -> Hour
+h24 value =
+    Hour <| (clamp 0 23 value - 12)
+
+
+{-| Add minutes to the hour to make it a Time.
+
+Example:
+
+    am 7 |> m 15 -- 7:15:00,000
+
+-}
+m : Int -> Hour -> Time
+m value (Hour hour) =
+    Time <| (hour * 60 + clamp 0 59 value) * 60000
+
+
+{-| Takes the hours and minutes of the time and adds the milliseconds.
+The previous value of milliseconds is overridden.
+So:
+
+    (h24 2 |> m 5 |> ms 100) |> ms 45 --> h24 2 |> m 5 |> ms 45
+
+We opted for this solution so it is not necessary to use ms, if you do not need it.
+Most problems do not need milliseconds.
+
+-}
+ms : Time -> Int -> Time
+ms (Time time) value =
+    Time <| (time // 1000) * 1000 + clamp 0 1000 value
+
+
+{-| Get the time for noon.
+-}
+noon : Time
+noon =
+    Time 0
+
+
+{-| Get the time for midnight.
+-}
+midnight : Time
+midnight =
+    Time (-12 * 3600000)
--- tests/Chrono/TimeTests.elm
+++ tests/Chrono/TimeTests.elm
@@ -1,9 +1,11 @@
 module Chrono.TimeTests exposing (all)
 
 import Chrono.Moment as Moment
+import Chrono.TestUtils exposing (..)
 import Chrono.Time exposing (..)
 import Chrono.TimeZone as TimeZone
 import Expect
+import Fuzz
 import Test exposing (..)
 import Time
 
@@ -17,4 +19,34 @@ all =
                     |> fromMoment TimeZone.utc
                     |> toMsSinceNoon
                     |> Expect.equal -43200000
+        , test "construct time using AM" <|
+            \() ->
+                am 7
+                    |> m 15
+                    |> toMsSinceNoon
+                    |> Expect.equal (-5 * 3600000 + 15 * 60000)
+        , test "construct time using PM" <|
+            \() ->
+                pm 7
+                    |> m 15
+                    |> toMsSinceNoon
+                    |> Expect.equal (7 * 3600000 + 15 * 60000)
+        , test "12 AM is midnight" <|
+            \() ->
+                am 12
+                    |> m 0
+                    |> Expect.equal midnight
+        , test "12 PM is noon" <|
+            \() ->
+                pm 12
+                    |> m 0
+                    |> Expect.equal noon
+        , fuzz (Fuzz.intRange 1 11) "am and h24 are interchangeable" <|
+            \hour ->
+                am hour
+                    |> Expect.equal (h24 hour)
+        , fuzz (Fuzz.intRange 1 11) "pm and h24 are interchangeable" <|
+            \hour ->
+                pm hour
+                    |> Expect.equal (h24 (12 + hour))
         ]
